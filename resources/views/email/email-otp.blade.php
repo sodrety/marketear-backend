850e1c2d274734e028d9c0647613b163f3fdee43
awaludin <awaludin.dev@gmail.com>
2023-06-14 01:48:51 +0700

    update email otp

--- resources/views/email/email-otp.blade.php
+++ resources/views/email/email-otp.blade.php
@@ -2,10 +2,14 @@
     <section class="section section-auth w-50">
         <div class="container-fluid">
             <div class="content">
-                <img src="https://res.cloudinary.com/dmjkiarn3/image/upload/v1683565844/reminder_vcnjbc.png" alt="reminder">
+                <img src="https://res.cloudinary.com/dmjkiarn3/image/upload/v1686681707/subscription-success_levgzp.png" alt="reminder">
+                <h5 class="fw-700" style="font-size: 40px;">Konfirmasi OTP</h5>
+                <p class="p-med">
+                    Untuk validasi akun kamu, silahkan masukkan OTP dibawah ini.
+                </p>
                 <h5 class="fw-700" style="font-size: 40px;">{{ $details['otp'] }}</h5>
                 <p class="p-med">
-                    Masukan OTP diatas untuk mengaktifkan akun anda.
+                    Kode berlaku selama 10 menit.
                 </p>
             </div>
         </div>
