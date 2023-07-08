1025beee7cc2088064f676f9d166f47908764afc
awaludin <awaludin.dev@gmail.com>
2023-07-09 01:43:39 +0700

    update email template

--- resources/views/email/email-otp.blade.php
+++ resources/views/email/email-otp.blade.php
@@ -1,17 +1,18 @@
 @component('mail::message')
-    <section class="section section-auth w-50">
-        <div class="container-fluid">
-            <div class="content">
-                <img src="https://res.cloudinary.com/dmjkiarn3/image/upload/v1686681707/subscription-success_levgzp.png" alt="reminder">
-                <h5 class="fw-700" style="font-size: 40px;">Konfirmasi OTP</h5>
-                <p class="p-med">
-                    Untuk validasi akun kamu, silahkan masukkan OTP dibawah ini.
-                </p>
-                <h5 class="fw-700" style="font-size: 40px;">{{ $details['otp'] }}</h5>
-                <p class="p-med">
-                    Kode berlaku selama 10 menit.
-                </p>
-            </div>
+    <div class="content text-center">
+        <img class="mb-30" src="https://res.cloudinary.com/dmjkiarn3/image/upload/v1688840288/email-logo_ydmeaq.png" alt="Marketer" />
+        <p class="p-med text-center mb-30"style="font-size: 14px;">
+            Masukkan kode OTP di Marketer untuk memvalidasi email.
+        </p>
+        <strong class="fw-700 d-block text-black" style="font-size: 18px;">Kode OTP Anda</strong>
+        <strong class="fw-700 d-block mb-30" style="font-size: 64px;color: #2F59BF;">{{ $details['otp'] }}</strong>
+        <p class="p-med text-center mb-20">
+        Password hanya berlaku 10 menit.<br/>
+        Mohon jangan bagikan kode rahasia ini kesiapa pun.
+        </p>
+        <div class="d-flex">
+            <img src="https://res.cloudinary.com/dmjkiarn3/image/upload/v1688840288/footer-img_dzewak.png" alt="footer image" />
+            <img src="https://res.cloudinary.com/dmjkiarn3/image/upload/v1688840289/footer-logo_b533d7.png" alt="footer logo" />
         </div>
-    </section>
+    </div>
 @endcomponent
